a31084756d9ca14e1d442614783e17f51c7ffbcd
Guillermo Mercado <guillermo.mercado26@gmail.com>
2026-07-08 22:43:27 -0400

    Se agrega nueva app catalogo
    feat: nueva app Catalogo con vistas y agregada a la app principal.
    fix: se modifica base.html para que el navbar quede pegado arriba.

--- tienda_videojuegos/catalogo/views.py
+++ tienda_videojuegos/catalogo/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+def lista_juegos(request):
+    juegos = [
+        {'nombre': 'Dogo Racing', 'precio': 29.99, 'plataforma': 'PC, PS5, Xbox Series X'},
+        {'nombre': 'Cyber Legends', 'precio': 49.99, 'plataforma': 'PC, PS5'},
+        {'nombre': 'Fantasy Quest', 'precio': 39.99, 'plataforma': 'PC, Nintendo Switch'},
+        {'nombre': 'Space Warriors', 'precio': 59.99, 'plataforma': 'PC, Xbox Series X'},
+        {'nombre': 'Mystic Kingdom', 'precio': 34.99, 'plataforma': 'PS5, Nintendo Switch'},
+        {'nombre': 'Battle Arena X', 'precio': 44.99, 'plataforma': 'PC, PS5, Xbox Series X'},
+        {'nombre': 'Ocean Explorer', 'precio': 24.99, 'plataforma': 'PC'},
+        {'nombre': 'Zombie Survival', 'precio': 19.99, 'plataforma': 'PC, PS4, Xbox One'},
+        {'nombre': 'Speed Rivals', 'precio': 54.99, 'plataforma': 'PS5, Xbox Series X'},
+        {'nombre': 'Dragon Chronicles', 'precio': 69.99, 'plataforma': 'PC, PS5, Xbox Series X, Nintendo Switch'}
+    ]
+    
+    contexto_catalogo_juegos={'lista_juegos': juegos}
+    return render(request, 'catalogo/lista-juegos.html', contexto_catalogo_juegos)
