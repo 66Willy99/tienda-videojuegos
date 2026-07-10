a38f5f9cf4a4cabbbca01bced5308bb21fa7a413
Guillermo Mercado <guillermo.mercado26@gmail.com>
2026-07-09 20:34:10 -0400

    Se implementa Modelo Juegos al admin de Django y se modifica html
    feat: se agrega modelo Juego a django admin
    refactor: catalogo.views ahora implementa paginacion
    feat: lista_juegos.html ahora tiene paginacion
    feat: base.html ahora trae condicionales para active-link

--- tienda_videojuegos/catalogo/views.py
+++ tienda_videojuegos/catalogo/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from catalogo.models import Juego
 
 # Create your views here.
 def lista_juegos(request):
-    juegos = Juego.objects.all()
-    contexto_catalogo_juegos={'lista_juegos': juegos}
+    juegos = Juego.objects.all().order_by('id') # Mantenemos un orden consistente
+    paginator = Paginator(juegos, 6) # Mostramos 6 juegos por pagina 
+    
+    # Obtenemos el numero de pagina desde la URL (?page=2)
+    page_number = request.GET.get('page')
+    
+    # Obtenemos los objetos de esa pagina
+    page_obj = paginator.get_page(page_number)
+    
+    # Pasamos la plantilla como 'lista_juegos'
+    contexto_catalogo_juegos={'lista_juegos': page_obj}
+    
     return render(request, 'catalogo/lista-juegos.html', contexto_catalogo_juegos)
