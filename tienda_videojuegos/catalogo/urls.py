ea55272c73080f6971f7a3fa8d6aa5aa8a3e68c4
Guillermo Mercado <guillermo.mercado26@gmail.com>
2026-07-12 23:08:44 -0400

    Nueva app Buscador y plantillas detalle juego
    feat: nueva app buscador

--- tienda_videojuegos/catalogo/urls.py
+++ tienda_videojuegos/catalogo/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.lista_juegos, name='catalogo')
+    path('', views.lista_juegos, name='catalogo'),
+    path('<int:pk>/', views.detalle_juego, name='detalle-juego')
 ]
